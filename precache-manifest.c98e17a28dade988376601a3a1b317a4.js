f7391a6083ae589d94687ced2aa7361a9a08b26c
SIvaram0108 <65078129+SIvaram0108@users.noreply.github.com>
2020-10-24 18:21:53 +0530

    Updates

---
+++ precache-manifest.c98e17a28dade988376601a3a1b317a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e434d6976043fe42951f94ddff29386e",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "3989b6a40de35a37861e",
+    "url": "/calculator/static/css/main.f0e7a7d6.chunk.css"
+  },
+  {
+    "revision": "ebfaa16fc3f70addbf36",
+    "url": "/calculator/static/js/2.7d333a3f.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/calculator/static/js/2.7d333a3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3989b6a40de35a37861e",
+    "url": "/calculator/static/js/main.d89b081d.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  }
+]);
